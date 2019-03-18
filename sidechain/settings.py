8c59235a8db3be44f9a91d6573dcdb1d57eaa58e
MichaelTaylor96 <michael96taylor@gmail.com>
2019-03-18 16:15:42 -0400

    Set up postgresql database

--- sidechain/settings.py
+++ sidechain/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'sidechain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sidechain',
+        'USER': 'sidechain',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
