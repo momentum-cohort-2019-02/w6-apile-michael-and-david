ad8ffcf3b8379fa5401d8e0bce278c047f1482eb
MichaelTaylor96 <michael96taylor@gmail.com>
2019-03-19 12:50:54 -0400

    Added Tag model (also in admin)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("tag_list", kwargs={"slug": self.slug})
+    
