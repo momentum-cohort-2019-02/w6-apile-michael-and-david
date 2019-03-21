4056a8b372ef0dcbe2885d4fe15a7f5369768a43
MichaelTaylor96 <michael96taylor@gmail.com>
2019-03-21 10:49:56 -0400

    Deploying to heroku

--- sidechain/settings.py
+++ sidechain/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,5 @@ STATIC_URL = '/static/'
 ACCOUNT_ACTIVATION_DAYS = 5
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+django_heroku.settings(locals())
