7f8a570616a02a9ecd256bf529687f14e91c7500
MichaelTaylor96 <michael96taylor@gmail.com>
2019-03-23 20:51:36 -0400

    Fixed a couple things with the upvotes

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('tagged/<slug:slug>/', views.tagged_list, name='tagged_list'),
-    path('<slug:slug>/favorite/', views.favorite_post, name='favorite_post')
+    path('<slug:slug>/like/', views.like_post, name='like_post')
 ]
